0c59c8d2fe43f0d8464fd221f1899653f0960c56
Alain Lemaire <jaxdid@gmail.com>
2018-02-25 23:53:29 +0000

    Sets up data fetching for stats data

--- components/ChartsDashboard.js
+++ components/ChartsDashboard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 import ActivityIndicator from './ActivityIndicator'
 
 class ChartsDashboard extends Component {
@@ -6,14 +7,44 @@ class ChartsDashboard extends Component {
     super(props)
 
     this.state = {
-      stillLoading: true
+      statsData: []
     }
 
-    this.getData()
+    this.getStatsData()
   }
 
-  getData () {
-    // get data from Blockchain APIs
+  getStatsData () {
+    const stats = [
+      { stat: 'market-price', decimalPlaces: 2 },
+      { stat: 'avg-block-size', decimalPlaces: 2 },
+      { stat: 'transactions-per-second', decimalPlaces: 2 },
+      { stat: 'mempool-size', decimalPlaces: 0 },
+      { stat: 'total-bitcoins', decimalPlaces: 0 }
+    ]
+
+    const promises = stats.map(({ stat }) => {
+      return axios.get(`https://api.blockchain.info/charts/${stat}?timespan=1days&format=json&cors=true`)
+    })
+
+    axios.all(promises)
+      .then(responses => {
+        if (responses.length) {
+          const statsData = responses.map(({ data }, index) => {
+            const dataRetrieved = data && data.values && data.values.length
+
+            return {
+              data: dataRetrieved ? data : null,
+              decimalPlaces: stats[index].decimalPlaces
+            }
+          })
+
+          this.setState({ statsData })
+        }
+      })
+      .catch(() => {
+        console.error(`Unable to fetch stats data.`)
+        return null
+      })
   }
 
   render () {
