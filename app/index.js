d1db78c21a010d49411198c616ac9f6e032f46d1
Alain Lemaire <jaxdid@gmail.com>
2018-02-25 19:22:16 +0000

    Adds initial component, ESLint updates and fixes

--- app/index.js
+++ app/index.js
@@ -1,15 +1,6 @@
-const React = require('react')
-const ReactDOM = require('react-dom')
-require('./index.less')
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.less'
+import ChartsDashboard from '../components/ChartsDashboard'
 
-class App extends React.Component {
-  render () {
-    return (
-      <div>
-        Hello World!
-      </div>
-    )
-  }
-}
-
-ReactDOM.render(<App />, document.getElementById('dashboard'))
+ReactDOM.render(<ChartsDashboard />, document.getElementById('root'))
---
+++ components/ChartsDashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class ChartsDashboard extends Component {
+  render () {
+    return (
+      <div className="stats-container">
+        Hello World!
+      </div>
+    )
+  }
+}
+
+export default ChartsDashboard
